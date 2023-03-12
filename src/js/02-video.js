eb7c8184b5c36aebbca4ba018227d94287865701
Karinkarllina <karinkhaliullina@gmail.com>
2023-03-12 18:32:54 +0200

    ДЗ-8.2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const currentTime = "videoplayer-current-time";
+
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+ 
+    player.on('play', function () {
+        console.log('played the video!');
+    });
+
+const getCurrentTime = function (event) {
+    localStorage.setItem(currentTime, event.seconds);
+};
+
+    player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+    player.setCurrentTime(localStorage.getItem(currentTime));
