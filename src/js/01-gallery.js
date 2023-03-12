ab10aff52ea73534df93ca37c1d6907af10a970d
Karinkarllina <karinkhaliullina@gmail.com>
2023-03-12 16:04:23 +0200

    ДЗ-8.1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+
+function createGalleryMarkup(item) {
+
+    return item.map(({ preview, original, description }) => {
+        return `
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`
+}).join('')
+};
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+});
